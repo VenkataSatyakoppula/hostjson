b5f548d59a1c6ebd8319cd237dda5c13b4623a8f
VenkataSatyakoppula <60220673+VenkataSatyakoppula@users.noreply.github.com>
2021-04-15 20:46:25 +0530

    host

---
+++ Q10json.js
@@ -0,0 +1,56 @@
+var CPU = [
+{  "Product_id":1,
+    "Name":"Intel i5 8th gen",
+    "Price":"120$",
+    "Stock":20,
+    "type":"CPU"
+},
+{
+    "Product_id":2,
+    "Name":"Intel i7 10th gen",
+    "Price":"100$",
+    "Stock":0,
+    "type":"CPU"
+},
+{  "Product_id":3,
+    "Name":"Nvidia GTX 1080ti",
+    "Price":"220$",
+    "Stock":7,
+    "type":"GPU"
+},
+{
+    "Product_id":4,
+    "Name":"Nvidia RTX 3090",
+    "Price":"500$",
+    "Stock":1,
+    "type":"GPU"
+},
+{  "Product_id":5,
+    "Name":"Ausus itx-3990 ",
+    "Price":"250$",
+    "Stock":30,
+    "type":"Motherboard"
+},
+{
+    "Product_id":6,
+    "Name":"Intel micro-itx",
+    "Price":"130$",
+    "Stock":60,
+    "type":"Motherboard"
+},
+{
+    "Product_id":7,
+    "Name":"Crucial 8GB",
+    "Price":"400$",
+    "Stock":5,
+    "type":"RAM"
+},
+
+{
+    "Product_id":8,
+    "Name":"HP 16GB",
+    "Price":"750$",
+    "Stock":4,
+    "type":"RAM"
+},
+]
